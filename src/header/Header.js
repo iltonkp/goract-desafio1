e578d135180908e85928baac2aca42949dc2b81b
iltonkp <iltonk.si@gmail.com>
2019-05-09 20:04:31 -0300

    GoReact - Desafio1 - iniciando o componente de header

---
+++ src/header/Header.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Header = props => <h1>Teste Header</h1>;
+
+export default Header;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-alert("teste");
+import React, { Component, Fragment } from "react";
+import { render } from "react-dom";
+
+import Header from "./header/Header";
+
+import "./style.scss";
+class App extends Component {
+  render() {
+    return (
+      <Fragment>
+        <Header />
+      </Fragment>
+    );
+  }
+}
+
+render(<App />, document.getElementById("app"));
